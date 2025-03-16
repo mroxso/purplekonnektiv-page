6a9c0e53780a74b388d731011d42c1a4974f502b
mr0x50 <24775431+mroxso@users.noreply.github.com>
2025-03-16 22:24:24 +0100

    swap Calendar and Social Feed cards for improved mobile layout

--- app/page.tsx
+++ app/page.tsx
@@ -12,25 +12,25 @@ export default function Home() {
   return (
     <main className="container mx-auto py-6 px-4">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        {/* 2/3 Card (Left) */}
-        <Card className="col-span-1 lg:col-span-2">
-          <CardHeader>
-            <CardTitle>Social Feed</CardTitle>
-            <CardDescription>Check out the latest posts on nostr</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Feed />
-          </CardContent>
-        </Card>
-
-        {/* 1/3 Card (Right) */}
-        <Card className="col-span-1">
+        {/* Calendar Card (shows first on mobile) */}
+        <Card className="col-span-1 order-first lg:order-last">
           <CardHeader>
             <CardTitle>Calendar</CardTitle>
             <CardDescription>Upcoming events and schedule</CardDescription>
           </CardHeader>
           <CardContent>
             {/* <Calendar mode="single" className="rounded-md border" /> */}
+          </CardContent>
+        </Card>
+
+        {/* Social Feed Card (shows second on mobile) */}
+        <Card className="col-span-1 lg:col-span-2 order-last lg:order-first">
+          <CardHeader>
+            <CardTitle>Social Feed</CardTitle>
+            <CardDescription>Check out the latest posts on nostr</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Feed />
           </CardContent>
         </Card>
       </div>
